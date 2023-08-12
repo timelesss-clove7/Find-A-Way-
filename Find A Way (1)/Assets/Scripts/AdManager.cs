a699369f8aedcd0efe53f488736d74514185449a
timelesss-clove7 <arora.dhruv714@gmail.com>
2023-08-12 19:56:01 +0530

    Add Changes

---
+++ Find A Way (1)/Assets/Scripts/AdManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour
+{
+    public static AdManager Instance; 
+
+     void Awake() {
+        if (Instance != null && Instance != this )
+        {
+            Destroy(gameObject);
+        }
+    }
+}
